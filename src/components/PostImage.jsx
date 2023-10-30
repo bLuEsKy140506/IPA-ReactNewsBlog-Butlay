fdce1824f46c7a20e864c9a6d4e35ff22c9dac6c
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-10-30 21:22:14 +0800

    updating codes particularly on add images

--- src/components/PostImage.jsx
+++ src/components/PostImage.jsx
@@ -39,8 +39,7 @@ export const PostImage = ({ addImageSuccessful }) => {
 
   return (
     <div>
-      {image ===
-        "https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg" && (
+      {image !== "" && (
         <FaRegWindowClose
           size={30}
           className="item-cross"
